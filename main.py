923e0e2ad1f30c17c59574bc0979c12fac9ea938
Johnprivas1 <130917128+Johnprivas1@users.noreply.github.com>
2023-06-16 19:00:37 -0500

    Add files via upload
    Ejercicio 5

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Problema 5
+"""
+
+import random_numbers
+
+random_list = random_numbers.generate_random_numbers()
+random_numbers.display_list(random_list)
+random_numbers.sort_and_display_list(random_list)
